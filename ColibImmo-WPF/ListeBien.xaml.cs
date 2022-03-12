f1552d133bda7a743b2f3e0a66ed49c15d56f8c2
fjquen <fjquen@gmail.com>
2022-03-12 18:59:18 +0100

    creation de la fonction ajout de bien dans .cs

--- ColibImmo-WPF/ListeBien.xaml.cs
+++ ColibImmo-WPF/ListeBien.xaml.cs
@@ -18,6 +18,8 @@ using System.IO;
 using System.Text.Json;
 using System.Reflection;
 using ColibImmo_WPF.Class;
+using System.Net.Http;
+using System.Text.Json.Nodes;
 
 namespace ColibImmo_WPF
 {
@@ -98,46 +100,37 @@ namespace ColibImmo_WPF
             }
         }
 
-        
 
-        /*public static Task<HttpResponseMessage>(object sender, MouseButtonEventArgs e)
+        private async void deleteProject(object sender, RoutedEventArgs e)
         {
-            /*Client api = new Client();
-            Stream? streamAPI = await api.DeleteAsync("/project/6");
-            if (streamAPI != null)
-            {
-                MessageBox.Show("suprimer");
-            }
-            else
-            {
-                MessageBox.Show("Erreur de connexion.");
-            }
-           
-            
-            /*else
-            {
-                MessageBox.Show("Erreur GetTypeProject.");
-            }
-            /*try
-            {
-                Client apiProjectByType = new Client();
-                Stream? streamAPIProjectByType = await apiProjectByType.GetCallAsync($"project/projectsByType/{typeProject[i].Id.ToString()}");
-                using (HttpClient client = new HttpClient())
-                {
-                    client.BaseAddress = new Uri(apiUrl);
-                    client.Timeout = TimeSpan.FromSeconds(900);
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                    var response = client.DeleteAsync(apiUrl);
-                    response.Wait();
-                    return response;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }*/
+            Button button = (Button)sender;
+            TextBlock textBlock = (TextBlock)button.Content;
+            var weburidelete = "http://api.colibimmo.cda.ve.manusien-ecolelamanu.fr/public/project/" + textBlock.Text;
+            HttpClient client = new HttpClient();
+            var res = await client.DeleteAsync(weburidelete);
+        }
 
+        public async void PostAsync(object sender, RoutedEventArgs e)
+        {
+            var project = new Project();
+            project.idTypeProject = 1;
+            project.idPerson = 5;
+            project.idPersonAgent = 9;
+            project.idAddress = 1;
+            var type_property = new Type_Property();
+            type_property.Id = 1;
 
+            var json = JsonSerializer.Serialize(project);
+
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var url = "http://api.colibimmo.cda.ve.manusien-ecolelamanu.fr/public/project";
+            using var client = new HttpClient();
+
+            var response = await client.PostAsync(url, data);
+
+            string result = response.Content.ReadAsStringAsync().Result;
+            MessageBox.Show(result);
+        }
     }
 }
