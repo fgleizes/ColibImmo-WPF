e7aab93e52042e97db3cebd7d3a377f867b355e9
fjquen <fjquen@gmail.com>
2022-05-03 18:26:16 +0200

    ajout classe role

---
+++ ColibImmo-WPF/API/JSON/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ColibImmo_WPF.API.JSON
+{
+    internal class Role
+    {
+        [JsonPropertyName("id")]
+        public int? Id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+
+        [JsonPropertyName("priority")]
+        public int? Priority { get; set; }
+    }
+}
