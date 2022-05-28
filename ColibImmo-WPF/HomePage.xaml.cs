70ea37d2f6d8a551bf0d8a6fdcfd1ef14e58b350
florent.gleizes <florent.gleizes@CK-5CG1172R9T>
2022-05-28 15:07:32 +0200

    cleaning homePage.cs

--- ColibImmo-WPF/HomePage.xaml.cs
+++ ColibImmo-WPF/HomePage.xaml.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ColibImmo_WPF
 {
-    /// <summary>
-    /// Logique d'interaction pour HomePage.xaml
-    /// </summary>
     public partial class HomePage : Page
     {
-        public System.Windows.CornerRadius CornerRadius { get; set; }
         public HomePage()
         {
             InitializeComponent();
@@ -30,8 +14,18 @@ namespace ColibImmo_WPF
 
         public void Welcome()
         {
-            TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
-            welcomeMessage.Text = $"Bienvenue {ti.ToTitleCase(Application.Current.Properties["firstname"].ToString())} {ti.ToTitleCase(Application.Current.Properties["lastname"].ToString())} sur ColibImmo";
+            TextInfo? ti = CultureInfo.CurrentCulture.TextInfo;
+            if(Application.Current.Properties != null)
+            {
+                string? firstname = Application.Current.Properties["firstname"]?.ToString();
+                string? lastname = Application.Current.Properties["lastname"]?.ToString();
+
+                if(firstname != null && lastname != null)
+                {
+                    welcomeMessage.Text = $"Bienvenue {ti.ToTitleCase(firstname)} {ti.ToTitleCase(lastname)} sur ColibImmo";
+                }
+            }
         }
     }
 }
+
